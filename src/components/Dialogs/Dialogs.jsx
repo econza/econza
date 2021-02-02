6e0fc7254032d4abbe890e8fc9347d32a0995bea
Заяц <zayatsslava@gmail.com>
2021-02-02 15:10:19 +0300

    lesson21 create css for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import s from './Dialogs.module.css'
+import React from "react";
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>Stefon</div>
+        <div className={s.dialog}>Marina</div>
+        <div className={s.dialog}>Vyacheslav</div>
+        <div className={s.dialog}>Jana</div>
+        <div className={s.dialog}>Alisa</div>
+        <div className={s.dialog}>Polina</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Give me money</div>
+        <div className={s.message}>lol</div>
+        <div className={s.message}>where is my mind?</div>
+      </div>
+    </div>
+  );
 };
 
-export default Dialogs
+export default Dialogs;
