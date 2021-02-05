13189f92316a3001ca099f26a467f66e59af87f5
Заяц <zayatsslava@gmail.com>
2021-02-05 13:31:34 +0300

    lesson 27 - 28 move data (messages posts ..) to index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,28 +4,12 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = (props) => {
-  let dialogs = [
-    { id: "1", name: "Stefon" },
-    { id: "2", name: "Marina" },
-    { id: "3", name: "Vyacheslav" },
-    { id: "4", name: "Jana" },
-    { id: "5", name: "Alisa" },
-    { id: "6", name: "Polina" },
-  ];
 
-  let messages = [
-    { id: "1", message: "HI" },
-    { id: "2", message: "lol" },
-    { id: "3", message: "where is my mind?" },
-    { id: "4", message: "what is love?" },
-    { id: "5", message: "Baby dont hurt me" },
-  ];
-
-  let dialogsElement = dialogs.map((d) => (
+  let dialogsElement = props.dialogs.map((d) => (
     <DialogItem name={d.name} id={d.id} />
   ));
 
-  let messageElement = messages.map((m) => <Message message={m.message} />);
+  let messageElement = props.messages.map((m) => <Message message={m.message} />);
 
   return (
     <div className={s.dialogs}>
