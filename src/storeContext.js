8b588e4b74f26a2319c867d480b679749781fbb1
Заяц <zayatsslava@gmail.com>
2021-02-17 12:16:31 +0300

    fix code at prev branch

--- src/storeContext.js
+++ src/storeContext.js
@@ -3,10 +3,10 @@ import React from "react";
 const StoreContext = React.createContext(null);
 
 export const Provider = (props) => {
-  return;
+  return(
   <StoreContext.Provider value={props.store}>
     {props.children}
-  </StoreContext.Provider>;
+  </StoreContext.Provider>)
 };
 
 export default StoreContext;
