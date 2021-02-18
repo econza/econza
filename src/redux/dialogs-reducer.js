64071b588c89db4f8206353f61f4a3b7008a10cd
Заяц <zayatsslava@gmail.com>
2021-02-18 16:11:44 +0300

    fix reducers for profile and dialogs

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -23,18 +23,20 @@ let initialState = {
 }
 
 const dialogsReducer = (state = initialState, action) => {
+  
   switch (action.type) {
-    case UPDATE_NEW_MESSAGE_BODY:{
-      let stateCopy = {...state}
-      stateCopy.newMessageBody = action.body;
-      return stateCopy;}
+    case UPDATE_NEW_MESSAGE_BODY:
+      return {
+      ...state,
+      newMessageBody: action.body
+    };
     case SEND_MESSAGE:
-     { let body = state.newMessageBody;
-      let stateCopy = {...state}
-      stateCopy.messages = [...state.messages];
-      stateCopy.messages.push(body);
-      stateCopy.newMessageBody = "";
-      return stateCopy;}
+      let body = state.newMessageBody;
+    return { 
+      ...state,
+      newMessageBody: "",
+      messages: [...state.messages, {id: 6, message: body}]
+      }
     default:
       return state;
   }
