f3a627d9a770f03fea3a7fac3a7b448bc3fc7ee6
Заяц <zayatsslava@gmail.com>
2021-02-24 14:19:39 +0300

    connect axios and create users query

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,64 +1,87 @@
-import React from "react"
-import styles from "./users.module.css"
+import React from "react";
+import styles from "./users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png"
 
 let Users = (props) => {
+  if (props.users.length === 0) {
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
+  }
 
-if (props.users.length === 0) {
-props.setUsers ([
-    {
-        id: "1",
-        photoUrl: 'https://www.meme-arsenal.com/memes/a538041abe14be401629711790bdd84d.jpg',
-        followed: false,
-        fullName: "Stefon",
-        status: "i am a lover",
-        location: { city: "Rostov-on-Don", country: "Russia" },
-      },
-      {
-        id: "2",
-        photoUrl: 'https://cdn.shopify.com/s/files/1/0023/0565/4882/products/6_1_1ed9c532-225b-49d5-a23f-fb7d6ee8fce7_1200x.jpg?v=1571698172',
-        followed: true,
-        fullName: "Yana",
-        status: "Whats up?",
-        location: { city: "Gomel", country: "Belarus" },
-      },
-      {
-        id: "3",
-        photoUrl: 'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkzFB3khwQLJh-v57INgQs76aKTM5SRkZCeTgDn6uOyic&fn=sqr_288',
-        followed: true,
-        fullName: "Marina",
-        status: "VIP",
-        location: { city: "Moscow", country: "Russia" },
-      },
-    ]
-)}
+  // props.setUsers ([
+  //     {
+  //         id: "1",
+  //         photoUrl: 'https://www.meme-arsenal.com/memes/a538041abe14be401629711790bdd84d.jpg',
+  //         followed: false,
+  //         fullName: "Stefon",
+  //         status: "i am a lover",
+  //         location: { city: "Rostov-on-Don", country: "Russia" },
+  //       },
+  //       {
+  //         id: "2",
+  //         photoUrl: 'https://cdn.shopify.com/s/files/1/0023/0565/4882/products/6_1_1ed9c532-225b-49d5-a23f-fb7d6ee8fce7_1200x.jpg?v=1571698172',
+  //         followed: true,
+  //         fullName: "Yana",
+  //         status: "Whats up?",
+  //         location: { city: "Gomel", country: "Belarus" },
+  //       },
+  //       {
+  //         id: "3",
+  //         photoUrl: 'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1RkzFB3khwQLJh-v57INgQs76aKTM5SRkZCeTgDn6uOyic&fn=sqr_288',
+  //         followed: true,
+  //         fullName: "Marina",
+  //         status: "VIP",
+  //         location: { city: "Moscow", country: "Russia" },
+  //       },
+  //     ]
+  // )}
 
-return <div>
-{
-    props.users.map(u => <div key={u.id}>
-        <span>
+  return (
+    <div>
+      {props.users.map((u) => (
+        <div key={u.id}>
+          <span>
             <div>
-                <img src={u.photoUrl} className={styles.userPhoto} />
+              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
             </div>
             <div>
-                {u.followed 
-                ? <button onClick={ () => { props.unfollow(u.id) } }>Unfollow</button> 
-                : <button onClick={ () => { props.follow(u.id)}  }>Follow</button>}
-                
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
             </div>
-        </span>
-        <span>
+          </span>
+          <span>
             <span>
-                <div>{u.fullName}</div>
-                <div>{u.status}</div>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
             </span>
             <span>
-                <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
-        </span>
-    </div>)
-}
-</div>
-}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Users;
