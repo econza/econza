2ed426e3ad1716ba87486416e10ef0e25d61ae66
Заяц <zayatsslava@gmail.com>
2021-03-18 17:22:26 +0300

    fix status change in profilestatus

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Profile from "./Profile";
-import { getUserProfile } from "../../redux/profile-reducer";
+import { getUserProfile, getStatus, updateStatus } from "../../redux/profile-reducer";
 import { connect } from "react-redux";
 import { Redirect, withRouter } from "react-router-dom";
 import { withAuthRedirect } from "../../hoc/withAuthRedirect";
@@ -12,13 +12,14 @@ class ProfileContainer extends React.Component {
     let userId = this.props.match.params.userId;
     if (!userId) { userId = 2; }
 this.props.getUserProfile(userId);
+this.props.getStatus(userId);
   }
 
   render() {
    
     return (
       <div>
-        <Profile {...this.props} profile={this.props.profile}  />
+        <Profile {...this.props} profile={this.props.profile} status={this.props.status} />
       </div>
     );
   }
@@ -26,10 +27,11 @@ this.props.getUserProfile(userId);
 
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
+  status: state.profilePage.status,
 });
 
 export default compose(
-  connect(mapStateToProps, { getUserProfile }),
+  connect(mapStateToProps, { getUserProfile, getStatus, updateStatus}),
   withRouter,
   // withAuthRedirect
 )(ProfileContainer);
