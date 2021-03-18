68df7a29747d7ee9c934984d7027a562da8c1297
Заяц <zayatsslava@gmail.com>
2021-03-18 12:58:57 +0300

    fix profile status

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
 
+class ProfileStatus extends React.Component {
+  state = {
+    editMode: false,
+    title: "YO",
+  };
 
-const ProfileStatus = (props) => {
- 
-  
+  activateEditMode() {
+      this.setState( {
+        editMode: true
+      } )
+  };
 
-  return (
-    <div>
-        <div>
-            <span>{props.status}</span>
-        </div>
-        <div>
-            <input value={props.status}/>
-        </div>
-    </div>
-  );
+  deactivateEditMode() {
+    this.setState( {
+      editMode: false
+    } )
 };
+
+  render() {
+    return (
+      <div>
+        {!this.state.editMode && (
+          <div>
+            <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+          </div>
+        )}
+        {this.state.editMode && (
+          <div>
+            <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status} />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default ProfileStatus;
