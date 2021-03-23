a2b4b10990531f34c7853c7a3b304486827985e8
Заяц <zayatsslava@gmail.com>
2021-03-23 17:04:46 +0300

    crate form contrls and validators

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { required } from "../../utils/validators/validators";
+import { Input } from "../common/FormsControls/FormsControls";
 
 const LoginForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
-        <Field placeholder={"Login"} name={"login"} component={"input"} />
+        <Field
+          placeholder={"Login"}
+          validate={[required]}
+          name={"login"}
+          component={Input}
+        />
       </div>
       <div>
-        <Field placeholder={"Password"} name={"password"} component={"input"} />
+        <Field
+          placeholder={"Password"}
+          validate={[required]}
+          name={"password"}
+          component={Input}
+        />
       </div>
       <div>
-        <Field  component={"input"} name={"rememberMe"} type={"checkbox"} />
+        <Field component={Input} name={"rememberMe"} type={"checkbox"} />
         remember me
       </div>
       <div>
@@ -24,14 +36,12 @@ const LoginForm = (props) => {
 const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
 
 const Login = (props) => {
- const onSubmit = (formData ) => {
-
- }
+  const onSubmit = (formData) => {};
 
   return (
     <div>
       <h1>Login</h1>
-      <LoginReduxForm onSubmit={onSubmit}/>
+      <LoginReduxForm onSubmit={onSubmit} />
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,44 +2,44 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import {maxLengthCreator, required} from "../../../utils/validators/validators";
+import { Textarea } from "../../common/FormsControls/FormsControls";
+
+const maxLenght10 = maxLengthCreator(10);
 
 const MyPosts = (props) => {
   let postsElement = props.posts.map((p) => (
     <Post message={p.message} likesCount={p.likesCount} />
   ));
 
-  // let newPostElement = React.createRef();
-
   let onAddPost = (values) => {
     props.addPost(values.newPostText);
   };
 
-
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
-      <AddNewPostFormRedux onSubmit={onAddPost}/>
+      <AddNewPostFormRedux onSubmit={onAddPost} />
       <div className={s.posts}>{postsElement}</div>
     </div>
   );
 };
 
-
 const AddNewPostForm = (props) => {
- return (
-  <form onSubmit={props.handleSubmit}>
-  <div>
-    <Field name="newPostText" component="textarea"/>
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field name="newPostText" component={Textarea} validate={[required, maxLenght10]} placeholder={"Post message"}/>
+      </div>
+      <div>
+        <button>Add post</button>
+      </div>
+    </form>
+  );
+};
 
-  </div>
-  <div>
-    <button>Add post</button>
-  </div>
-</form>
- )
-}
-
-const AddNewPostFormRedux = reduxForm({ form: "profileAddNewPostForm" })(AddNewPostForm);
-
+const AddNewPostFormRedux = reduxForm({ form: "profileAddNewPostForm" })(
+  AddNewPostForm
+);
 
 export default MyPosts;
