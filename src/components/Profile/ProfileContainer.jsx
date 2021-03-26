8d66f85b93df35d0b6e6679ca52f27c77c44f823
Заяц <zayatsslava@gmail.com>
2021-03-26 22:34:19 +0300

    fix componentDidMount in profileContainer

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -12,15 +12,18 @@ import { compose } from "redux";
 class ProfileContainer extends React.Component {
   componentDidMount() {
     let userId = this.props.match.params.userId;
+    if (userId) {
+      this.props.getUserProfile(userId);
+      this.props.getStatus(userId);
+
     if (!userId) {
-      userId = this.props.authorizedUserId;
+      userId = this.props.authorizedUserId;}
+      
       if (!userId) {
         this.props.history.push("/login");
       }
     }
-    if (userId) {
-    this.props.getUserProfile(userId);
-    this.props.getStatus(userId);}
+    
   }
 
   render() {
