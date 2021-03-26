29e7ad151b48cde2d6f8e998d7a13fc3709e2c46
Заяц <zayatsslava@gmail.com>
2021-03-26 13:40:42 +0300

    add logins errors nedd to fix

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import Profile from "./Profile";
-import { getUserProfile, getStatus, updateStatus } from "../../redux/profile-reducer";
+import {
+  getUserProfile,
+  getStatus,
+  updateStatus,
+} from "../../redux/profile-reducer";
 import { connect } from "react-redux";
-import { Redirect, withRouter } from "react-router-dom";
-import { withAuthRedirect } from "../../hoc/withAuthRedirect";
+import { withRouter } from "react-router-dom";
 import { compose } from "redux";
-
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
     let userId = this.props.match.params.userId;
-    if (!userId) { userId = 2; }
-this.props.getUserProfile(userId);
-this.props.getStatus(userId);
+    if (!userId) {
+      userId = this.props.authorizedUserId;
+    }
+    this.props.getUserProfile(userId);
+    this.props.getStatus(userId);
   }
 
   render() {
-   
     return (
       <div>
-        <Profile {...this.props} profile={this.props.profile} status={this.props.status} />
+        <Profile
+          {...this.props}
+          profile={this.props.profile}
+          status={this.props.status}
+          updateStatus={this.props.updateStatus}
+        />
       </div>
     );
   }
@@ -28,15 +36,12 @@ this.props.getStatus(userId);
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
   status: state.profilePage.status,
+  authorizedUserId: state.auth.userId,
+  isAuth: state.auth.isAuth,
 });
 
 export default compose(
-  connect(mapStateToProps, { getUserProfile, getStatus, updateStatus}),
-  withRouter,
+  connect(mapStateToProps, { getUserProfile, getStatus, updateStatus }),
+  withRouter
   // withAuthRedirect
 )(ProfileContainer);
-
-
-
-
-
