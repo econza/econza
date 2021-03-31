e7561145546c450b52700897154b7266c8b5925d
Заяц <zayatsslava@gmail.com>
2021-03-31 16:15:43 +0300

    add react memo in my posts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,7 +7,7 @@ import { Textarea } from "../../common/FormsControls/FormsControls";
 
 const maxLenght10 = maxLengthCreator(10);
 
-const MyPosts = (props) => {
+const MyPosts = React.memo((props) => {
   let postsElement = props.posts.map((p) => (
     <Post message={p.message} likesCount={p.likesCount} />
   ));
@@ -23,7 +23,7 @@ const MyPosts = (props) => {
       <div className={s.posts}>{postsElement}</div>
     </div>
   );
-};
+});
 
 const AddNewPostForm = (props) => {
   return (
